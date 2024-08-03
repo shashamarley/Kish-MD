fcba12fe782e49531f8267c3656263622d26a71d
shashamarley <149763765+shashamarley@users.noreply.github.com>
2024-08-03 13:41:31 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['254745936840']
-global.ownername = "KISH"//owner name
+global.ownernumber = ['254753295691']
+global.ownername = "undefined™"//owner name
 global.ytname = "YT: Kish-MD"
 global.socialm = "GitHub: Kish-MD"
 global.location = "Kenya"

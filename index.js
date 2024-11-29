d98ec166afc4fbf82d4d3717b1cae8ea67fca575
shashamarley <149763765+shashamarley@users.noreply.github.com>
2024-11-29 17:38:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
+const { spawn } = require('child_process');
+const path = require('path');
 
+// Function to start the child process
 function start() {
-   let args = [path.join(__dirname, 'plugins.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot..')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+    const args = [path.join(__dirname, 'plugins.js'), ...process.argv.slice(2)];
+    console.log(Starting bot with args: ${args.join(' ')});
+
+    const childProcess = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+    });
+
+    childProcess.on('message', (data) => {
+        if (data === 'reset') {
+            console.log('Restarting Bot...');
+            childProcess.kill();
+            start();
+        }
+    });
+
+    childProcess.on('exit', (code) => {
+        console.error(Child process exited with code: ${code});
+        if ([0, 1].includes(code)) {
+            console.log('Restarting due to exit code...');
+            start();
+        }
+    });
+
+    childProcess.on('close', () => {
+        console.log('Child process closed. Cleaning up...');
+    });
 }
-start()
+
+start();
